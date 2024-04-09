ad6c487369c1498dfe1de74048aaa32d7455bcf1
irais-valenzuela <iraisvq8@gmail.com>
2024-04-08 17:21:03 -0700

    learning the values

--- src/main.go
+++ src/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, Go!")
+  fmt.Println("Hello World!")
+  fmt.Println("Go" + "lang")
+  fmt.Println("1 + 4 = ", 1 + 4)
+  fmt.Println("false", true && false)
+  fmt.Println("true", true || false)
+  fmt.Println(!true)
 }
