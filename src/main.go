cbe959d50cd268736e7ba8b0028154974450ecf6
irais-valenzuela <iraisvq8@gmail.com>
2024-04-10 14:21:48 -0700

    new go lessons

--- src/main.go
+++ src/main.go
@@ -3,10 +3,34 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Hello World!")
-  fmt.Println("Go" + "lang")
-  fmt.Println("1 + 4 = ", 1 + 4)
-  fmt.Println("false", true && false)
-  fmt.Println("true", true || false)
-  fmt.Println(!true)
+  // fmt.Println("Hello World!")
+  // fmt.Println("Go" + "lang")
+  // fmt.Println("1 + 4 = ", 1 + 4)
+  // fmt.Println("false", true && false)
+  // fmt.Println("true", true || false)
+  // fmt.Println(!true)
+  var num1 = 1
+  fmt.Println("1", num1)
+
+  var num2, num3 int = 2, 3
+  fmt.Println("2 3 === ", num2, num3)
+
+  var zero int
+  fmt.Println("0", zero)
+
+  var false bool
+  fmt.Println("false?", false)
+
+  var emptyString string 
+  fmt.Println("0", len(emptyString))
+
+  lastVar := "last variable"
+
+  fmt.Println(lastVar)
+
+  lastVar = "new value to prove a point lol"
+
+  fmt.Println(lastVar)
+
+
 }
