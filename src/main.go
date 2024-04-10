e16d94095dcfe9507680bad5ab605521f8953cca
irais-valenzuela <iraisvq8@gmail.com>
2024-04-10 15:05:50 -0700

    new for loop with a range

--- src/main.go
+++ src/main.go
@@ -4,45 +4,47 @@ import "fmt"
 
 func main() {
 
-  // printing 
+	// printing
 
-  // fmt.Println("Hello World!")
-  // fmt.Println("Go" + "lang")
-  // fmt.Println("1 + 4 = ", 1 + 4)
-  // fmt.Println("false", true && false)
-  // fmt.Println("true", true || false)
-  // fmt.Println(!true)
+	// fmt.Println("Hello World!")
+	// fmt.Println("Go" + "lang")
+	// fmt.Println("1 + 4 = ", 1 + 4)
+	// fmt.Println("false", true && false)
+	// fmt.Println("true", true || false)
+	// fmt.Println(!true)
 
-  // variable declaration + shorthand declaration and initialization
+	// variable declaration + shorthand declaration and initialization
 
-  // var num1 = 1
-  // fmt.Println("1", num1)
+	// var num1 = 1
+	// fmt.Println("1", num1)
 
-  // var num2, num3 int = 2, 3
-  // fmt.Println("2 3 === ", num2, num3)
+	// var num2, num3 int = 2, 3
+	// fmt.Println("2 3 === ", num2, num3)
 
-  // var zero int
-  // fmt.Println("0", zero)
+	// var zero int
+	// fmt.Println("0", zero)
 
-  // var false bool
-  // fmt.Println("false?", false)
+	// var false bool
+	// fmt.Println("false?", false)
 
-  // var emptyString string 
-  // fmt.Println("0", len(emptyString))
+	// var emptyString string
+	// fmt.Println("0", len(emptyString))
 
-  // lastVar := "last variable"
+	// lastVar := "last variable"
 
-  // fmt.Println(lastVar)
+	// fmt.Println(lastVar)
 
-  // lastVar = "new value to prove a point lol"
+	// lastVar = "new value to prove a point lol"
 
-  // fmt.Println(lastVar)
+	// fmt.Println(lastVar)
 
-  // for loops
+	// for loops
 
-  for i:=0; i<=5; i++ {
-    fmt.Println(i)
-  }
+	for i := 0; i <= 5; i++ {
+		fmt.Println(i)
+	}
 
-
+	for i := range 3 {
+		fmt.Println("range", i)
+	}
 }
