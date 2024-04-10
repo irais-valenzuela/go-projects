ecb3de3db93d0968f6a496fda7068b60ca39370e
irais-valenzuela <iraisvq8@gmail.com>
2024-04-10 14:56:32 -0700

    my first for loop in go!

--- src/main.go
+++ src/main.go
@@ -3,34 +3,46 @@ package main
 import "fmt"
 
 func main() {
+
+  // printing 
+
   // fmt.Println("Hello World!")
   // fmt.Println("Go" + "lang")
   // fmt.Println("1 + 4 = ", 1 + 4)
   // fmt.Println("false", true && false)
   // fmt.Println("true", true || false)
   // fmt.Println(!true)
-  var num1 = 1
-  fmt.Println("1", num1)
 
-  var num2, num3 int = 2, 3
-  fmt.Println("2 3 === ", num2, num3)
+  // variable declaration + shorthand declaration and initialization
 
-  var zero int
-  fmt.Println("0", zero)
+  // var num1 = 1
+  // fmt.Println("1", num1)
 
-  var false bool
-  fmt.Println("false?", false)
+  // var num2, num3 int = 2, 3
+  // fmt.Println("2 3 === ", num2, num3)
 
-  var emptyString string 
-  fmt.Println("0", len(emptyString))
+  // var zero int
+  // fmt.Println("0", zero)
 
-  lastVar := "last variable"
+  // var false bool
+  // fmt.Println("false?", false)
 
-  fmt.Println(lastVar)
+  // var emptyString string 
+  // fmt.Println("0", len(emptyString))
 
-  lastVar = "new value to prove a point lol"
+  // lastVar := "last variable"
 
-  fmt.Println(lastVar)
+  // fmt.Println(lastVar)
+
+  // lastVar = "new value to prove a point lol"
+
+  // fmt.Println(lastVar)
+
+  // for loops
+
+  for i:=0; i<=5; i++ {
+    fmt.Println(i)
+  }
 
 
 }
