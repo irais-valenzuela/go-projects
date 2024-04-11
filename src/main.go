535a2a00505e42cbaf26c54588dd2b4fff947a5b
irais-valenzuela <iraisvq8@gmail.com>
2024-04-11 13:11:58 -0700

    arrays in go

--- src/main.go
+++ src/main.go
@@ -6,66 +6,67 @@ func main() {
 
 	// printing
 
-	// fmt.Println("Hello World!")
-	// fmt.Println("Go" + "lang")
-	// fmt.Println("1 + 4 = ", 1 + 4)
-	// fmt.Println("false", true && false)
-	// fmt.Println("true", true || false)
-	// fmt.Println(!true)
+	fmt.Println("Hello World!")
+	fmt.Println("Go" + "lang")
+	fmt.Println("1 + 4 = ", 1 + 4)
+	fmt.Println("false", true && false)
+	fmt.Println("true", true || false)
+	fmt.Println(!true)
 
 	// variable declaration + shorthand declaration and initialization
 
-	// var num1 = 1
-	// fmt.Println("1", num1)
+	var num1 = 1
+	fmt.Println("1", num1)
 
-	// var num2, num3 int = 2, 3
-	// fmt.Println("2 3 === ", num2, num3)
+	var num2, num3 int = 2, 3
+	fmt.Println("2 3 === ", num2, num3)
 
-	// var zero int
-	// fmt.Println("0", zero)
+	var zero int
+	fmt.Println("0", zero)
 
-	// var false bool
-	// fmt.Println("false?", false)
+	var false bool
+	fmt.Println("false?", false)
 
-	// var emptyString string
-	// fmt.Println("0", len(emptyString))
+	var emptyString string
+	fmt.Println("0", len(emptyString))
 
-	// lastVar := "last variable"
+	lastVar := "last variable"
 
-	// fmt.Println(lastVar)
+	fmt.Println(lastVar)
 
-	// lastVar = "new value to prove a point lol"
+	lastVar = "new value to prove a point lol"
 
-	// fmt.Println(lastVar)
+	fmt.Println(lastVar)
 
 	// for loops
 
-	// for i := 0; i <= 5; i++ {
-	// 	fmt.Println(i)
-	// }
+	for i := 0; i <= 5; i++ {
+		fmt.Println(i)
+	}
 
-	// for i := range 3 {
-	// 	fmt.Println("range", i)
-	// }
+	for i := range 3 {
+		fmt.Println("range", i)
+	}
 
-	// // if else chains
-	// if num := 9; num < 0 {
-	// 	fmt.Println(num, "number is negative")
-	// } else if num < 10 {
-	// 	fmt.Println(num, "number has 1 digit")
-	// } else {
-	// 	fmt.Println(num, "number has multiple digits")
-	// }
+	// if else chains
+	if num := 9; num < 0 {
+		fmt.Println(num, "number is negative")
+	} else if num < 10 {
+		fmt.Println(num, "number has 1 digit")
+	} else {
+		fmt.Println(num, "number has multiple digits")
+	}
 
 	// all three lessons in one
 	const two = 2.0
 
-	// for i := 0; i <= 5; i++ {
-	// 	if float64(i)/two == 2.0 {
-	// 		fmt.Println("should be 4", i)
-	// 	}
-	// }
-  
+	for i := 0; i <= 5; i++ {
+		if float64(i)/two == 2.0 {
+			fmt.Println("should be 4", i)
+		}
+	}
+
+  // switches
 	const day = 4
 
 	switch day {
@@ -84,4 +85,19 @@ func main() {
 	default:
 		fmt.Println("Today is Sunday")
 	}
+
+  // arrays
+  var array [6]int
+
+  fmt.Println("initial array", array)
+
+  count:= 1
+
+  for i:= 0; i < len(array); i++ {
+    array[i] = count
+    count += 1
+  }
+
+  fmt.Println("after assigning elems", array)
+    
 }
